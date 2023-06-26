b30c13251589a4986146556117bd699f023658b8
Houstin <houstin.angwenyi@student.moringaschool.com>
2023-06-26 08:59:40 +0300

    updated index.js

--- README.md
+++ README.md
@@ -4,4 +4,4 @@
 It is an app where you can vote for the cutest animal.It provides a fun way for people to express their preferences and engage with a community of animal lovers.The app features a user-interface that allows users to browse through cute animals and cast their votes for the cutest animal.
 ## Project Setup:
 ## Author & License:
-This is authored by Houstin angwenyi and is licensed by MIT 
+This is authored by Houstin angwenyi and is licensed undr the MIT License 
--- src/index.js
+++ src/index.js
@@ -1,34 +1,57 @@
 // fetch the characters data from the api
-fetch('db.json')
+function fetchCharacterData(){
+return fetch(' db.json')
 .then(response=>response.json())
 .then(data=>{
     const characterContainer=document.getElementById('characters')
+    const detailsContainers=document.getElementById('charactersDetails')
 
 //loop through each character and create a card to display it
-data.characters.forEach(characters=>{
-    const characterCard=document.createElement('div')
-    characterCard.classList.add('character')
+   data.characters.forEach(characters =>{
+    const characterName=document.createElement('div')
+    characterName.classList.add('character')
+    
+    characterName.textContent=characters.name
+
+    characterName.addEventListener("click",()=>{
+    characterDetails(characters)
+   })
+
+    characterContainer.appendChild(characterName) 
+  })
+})
+}
+//fuction to display the details of a character
+function characterDetails(characters){
+   
+    detailsContainers.innerHTML=""
 
     const characterImage=document.createElement('img')
     characterImage.src=characters.image
 
-    const characterName =document.createElement('div')
-    characterName.classList.add('character-name')
-    characterName.textContent=characters.name 
+    const characterName=document.createElement('h2')
+    characterName.textContent=characters.name
 
-    const characterVotes=document.createElement('div')
-    characterVotes.classList.add('character-votes')
-    characterVotes.textContent=`votes: ${characters.votes}`
+    const characterVotes=document.createElement('p')
+    characterVotes.textContent=`Votes: ${characters.votes}`
 
-    characterContainer.appendChild(characterName)
-    characterCard.appendChild(characterImage)
+    const voteButton=document.createElement('button')
+    voteButton.innerText='vote'
+
+    voteButton.addEventListener('click', () =>{
+        characters.vote++;
+        characterVotes.innerText= `Votes:${characters.votes}`
+    })
+
+    const detailsContainers=document.createElement('div')
+    detailsContainers.classList.add('details')
+    detailsContainers.appendChild(characterImage)
+    detailsContainers.appendChild(characterVotes)
+    detailsContainers.appendChild(voteButton)
     
-    characterCard.appendChild(characterVotes)
-
-    characterContainer.appendChild(characterCard)
-})
-})
-
-//fuction to display the details of a character
-function characterDetails(character){
-} 
+detailsContainer.appendChild(detailsContainers)  
+}
+//fetch character data and render the character list on page load
+   window.addEventListener('DOMContentLoaded',() => {
+    fetchCharacterData()
+   })
