4c00ae28672565b033846c47e35ca5356bd18d04
Houstin <houstin.angwenyi@student.moringaschool.com>
2023-06-29 00:51:46 +0300

    updated js that displays details

--- index.js
+++ index.js
@@ -1,51 +1,50 @@
-document.addEventListener('DOMContentLoaded', () => {
-    // fetch data from the server
-    fetch("db.json")
-      .then(response => response.json())
-      .then(data => {
-        const characterContainer = document.getElementById('characters');
-        const detailsContainer = document.getElementById('charactersDetails');
-        // loop through each character and create a card to display it
-        data.characters.forEach(characters => {
-          const characterName = document.createElement('div');
-          characterName.classList.add('characters');
-          characterName.textContent = characters.name;
-          characterName.addEventListener("click", () => {
-            characterDetails(characters, detailsContainer); // pass detailsContainer as an argument
-          });
-          characterContainer.appendChild(characterName);
+document.addEventListener("DOMContentLoaded", () => {
+  const characterContainer = document.getElementById('character');
+  const detailsContainer = document.getElementById('characterDetails');
+
+  // fetch data from the server
+  fetch("http://localhost:3000/characters")
+    .then(response => response.json())
+    .then(data => {
+      // loop through each character and create list
+      data.forEach(characters => {
+        const characterElement = document.createElement('li');
+        characterElement.textContent = characters.name;
+        characterContainer.appendChild(characterElement);
+
+        characterElement.addEventListener("click", (e) => {
+          e.preventDefault()
+          characterDetails(characters)//call character details function
         });
+        
       });
+      //
+      function characterDetails(character) {
+        // Clear previous details
+        detailsContainer.innerHTML = '';
+    
+        
+        const nameElement = document.createElement('h3');
+        nameElement.textContent = character.name;
+        detailsContainer.appendChild(nameElement);
+    
+        const imageElement = document.createElement('img');
+        imageElement.src = character.image;
+        imageElement.alt = character.name;
+        detailsContainer.appendChild(imageElement);
+    
+        const votesElement = document.createElement('p');
+        votesElement.textContent = `Votes: ${character.votes}`;
+        detailsContainer.appendChild(votesElement);
+        
+        
+        
+        
+      }
+    });
+    
+    //
+ 
   
-    // function to display the details of a character
-    function characterDetails(characters, detailsContainer) {
-      detailsContainer.innerHTML = "";
-  
-      // display image
-      const characterImage = document.createElement('img');
-      characterImage.src = characters.image;
-      detailsContainer.appendChild(characterImage);
-  
-      // display name
-      const characterName = document.createElement('h4');
-      characterName.textContent = characters.name;
-      detailsContainer.appendChild(characterName);
-  
-      // display votes
-      const characterVotes = document.createElement('p');
-      characterVotes.textContent = `Votes: ${characters.votes}`;
-      detailsContainer.appendChild(characterVotes);
-  
-      // create vote button
-      const voteButton = document.createElement('button');
-      voteButton.innerText = 'vote';
-  
-      voteButton.addEventListener('click', () => {
-        characters.vote++;
-        characterVotes.innerText = `Votes: ${characters.votes}`;
-      });
-  
-      detailsContainer.appendChild(voteButton);
-    }
-  });
-  
+
+})
