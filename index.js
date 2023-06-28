f2d0697cf3dcdd1741b329a3d02c49c376ddb6bc
Houstin <houstin.angwenyi@student.moringaschool.com>
2023-06-29 02:36:31 +0300

    completed

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const characterContainer = document.getElementById('character');
-    const detailsContainer = document.getElementById('characterDetails');
+    const characterContainer = document.getElementById('character')
+    const detailsContainer = document.getElementById('characterDetails')
     const formContainer=document.getElementById("formContainer")
 
     // fetch data from the server
@@ -9,34 +9,34 @@ document.addEventListener("DOMContentLoaded", () => {
     .then(data => {
       // loop through each character and create list
       data.forEach(characters => {
-        const characterElement = document.createElement('li');
-        characterElement.textContent = characters.name;
-        characterContainer.appendChild(characterElement);
+        const characterElement = document.createElement('li')
+        characterElement.textContent = characters.name
+        characterContainer.appendChild(characterElement)
 
         characterElement.addEventListener("click", (e) => {
           e.preventDefault()
           characterDetails(characters)//call character details function
-        });
+        })
         
-      });
+      })
       //function to display details
       function characterDetails(character) {
         // Clear previous details
         detailsContainer.innerHTML = '';
     
         
-        const nameElement = document.createElement('h3');
-        nameElement.textContent = character.name;
+        const nameElement = document.createElement('h3')
+        nameElement.textContent = character.name
         detailsContainer.appendChild(nameElement);
     
-        const imageElement = document.createElement('img');
-        imageElement.src = character.image;
-        imageElement.alt = character.name;
-        detailsContainer.appendChild(imageElement);
+        const imageElement = document.createElement('img')
+        imageElement.src = character.image
+        imageElement.alt = character.name
+        detailsContainer.appendChild(imageElement)
     
-        const votesElement = document.createElement('p');
-        votesElement.textContent = `Votes: ${character.votes}`;
-        detailsContainer.appendChild(votesElement);
+        const votesElement = document.createElement('p')
+        votesElement.textContent = `Votes: ${character.votes}`
+        detailsContainer.appendChild(votesElement)
 
         const voteButton=document.createElement("button")
         voteButton.textContent="vote"
@@ -69,18 +69,18 @@ document.addEventListener("DOMContentLoaded", () => {
       addAnimal.appendChild(animalLabel)
       addAnimal.appendChild(animalInput)
 
-      const imageLabel = document.createElement('label');
-      imageLabel.textContent = 'Image URL:';
-      const imageInput = document.createElement('input');
-      imageInput.type = 'url';
-      imageInput.required = true;
-      addAnimal.appendChild(imageLabel);
-      addAnimal.appendChild(imageInput);
+      const imageLabel = document.createElement('label')
+      imageLabel.textContent = 'Image URL:'
+      const imageInput = document.createElement('input')
+      imageInput.type = 'url'
+      imageInput.required = true
+      addAnimal.appendChild(imageLabel)
+      addAnimal.appendChild(imageInput)
 
 
-      const addButton = document.createElement('button');
-      addButton.textContent = 'Add Animal';
-      addAnimal.appendChild(addButton);
+      const addButton = document.createElement('button')
+      addButton.textContent = 'Add Animal'
+      addAnimal.appendChild(addButton)
 
       addAnimal.addEventListener('submit', (e) => {
         e.preventDefault();
@@ -93,16 +93,16 @@ document.addEventListener("DOMContentLoaded", () => {
              votes: 0
         }
          // Add the new character to the list and display it
-         const characterElement = document.createElement('li');
-         characterElement.textContent = name;
-         characterContainer.appendChild(characterElement);
+         const characterElement = document.createElement('li')
+         characterElement.textContent = name
+         characterContainer.appendChild(characterElement)
 
          characterElement.addEventListener('click', () => {
              characterDetails(newCharacter);
-          });
+          })
          // Reset the form inputs
              animalInput.value = '';
              imageInput.value = '';
-    });
+    })
   })
 })
